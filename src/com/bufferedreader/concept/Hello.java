fb4320c9670de4f40f2bc083efcbcf45f46ce876
vinay <313.vinay@gmail.com>
2018-08-09 21:14:33 +0530

    2nd version finished

---
+++ src/com/bufferedreader/concept/Hello.java
@@ -0,0 +1,15 @@
+package com.bufferedreader.concept;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("added by vinayteam");
+System.out.println("master vinay");
+
+
+	}
+	
+	///hello added by master for 2nd release
+
+}
